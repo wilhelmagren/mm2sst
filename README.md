9a08c0fd6254cbde77f4fe5a2d0991bbf0239167
wilhelmagren <wilhelmagren98@gmail.com>
2025-03-13 20:28:04 +0100

    feat: ip searcher using std hashmap

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # mm2sst
-MaxMind to Static Search Tree
+
+MaxMind to Static Search Tree (**S+ trees**).
+
+Or maybe just a b-tree-map?
 
 - Static search trees: 40x faster than binary search [link](https://curiouscoding.nl/posts/static-search-tree/)
+- Rust HashMap performance [link](https://morestina.net/blog/1843/the-stable-hashmap-trap)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use chrono::prelude::*;
+use serde::{Serialize, Deserialize};
+
+use std::collections::HashMap;
+use std::net::Ipv4Addr;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NodeRecord {
+    geo_location: String,
+    utc: DateTime<Utc>,
+    is_latest: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct IpNode {
+    records: Vec<NodeRecord>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct IpSearcher {
+    inner: HashMap<Ipv4Addr, IpNode>,
+}
+
+impl IpSearcher {
+    pub fn new() -> Self {
+        IpSearcher { inner: HashMap::new() }
+    }
+
+    pub fn insert(&mut self, ip: Ipv4Addr, n: NodeRecord) {
+        if let Some(_) = self.inner.get(&ip) {
+            self.inner.get_mut(&ip).unwrap().records.push(n);
+        } else {
+            self.inner.insert(ip, IpNode { records: vec![n] });
+        }
+    }
+
+    pub fn from_hashmap(hm: HashMap<Ipv4Addr, IpNode>) -> Self {
+        IpSearcher { inner: hm }
+    }
+
+    pub fn query(&self, q: Ipv4Addr) -> Option<&IpNode> {
+        self.inner.get(&q)
+    }
+
+    pub fn queries(&self, q: Vec<Ipv4Addr>) -> Vec<Option<&IpNode>> {
+        q.iter().map(|q| self.query(*q)).collect()
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let mut ips = IpSearcher::new();
+    ips.insert(
+        Ipv4Addr::new(10, 51, 3, 41),
+        NodeRecord {
+            geo_location: "Stockholm, Sweden".to_string(),
+            utc: Utc::now(),
+            is_latest: true,
+        },
+    );
+    println!("{:?}", ips);
+
+    let q = vec![Ipv4Addr::new(10, 51, 3, 41), Ipv4Addr::new(127, 0, 0, 1)];
+    let r = ips.queries(q);
+    println!("{:?}", r);
+
+    let ser = serde_json::to_string(&ips).unwrap();
+    println!("{}", ser);
 }
